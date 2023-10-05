55b3861cb25e163ab24a059be1de22ad808d3ab3
hong@arch-pyton-dev <yunghunghuang984@gmail.com>
2023-10-06 02:48:22 +0800

    Test codes

--- mytimer/mytimer/urls.py
+++ mytimer/mytimer/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('timer/', include("timer.urls")),
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='timer/', permanent=True)),
 ]
